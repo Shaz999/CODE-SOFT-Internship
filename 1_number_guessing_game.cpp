f4440bbebfadf0f159f416380aab21b12e6d3adf
Shaz Alam <99272333+Shaz999@users.noreply.github.com>
2024-08-08 20:59:02 +0530

    1_number_guessing_game.cpp

--- 1_number_guessing_game.cpp
+++ 1_number_guessing_game.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <cstdlib>
-
-
 #include <ctime>
+
 using namespace std;
+
 int main() {
     srand(time(0));
     int randomNumber = rand() % 100 + 1; 
     int userGuess = 0;
 
-    std::cout << "Guess the number (between 1 and 100): ";
+    cout << "Guess the number (between 1 and 100): ";
 
-    while (userGuess != randomNumber) {
+    while (true) {
         cin >> userGuess;
-        
+
         if (userGuess > randomNumber) {
             cout << "Too high! Try again: ";
         } else if (userGuess < randomNumber) {
-          cout << "Too low! Try again: ";
+            cout << "Too low! Try again: ";
         } else {
-            cout << "Congratulations! You guessed it!" <<endl;
+            cout << "Congratulations! You guessed it!" << endl;
+            break; 
         }
     }
 
